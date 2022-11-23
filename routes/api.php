d08168fcfbb60427004fa8e858bc872c5d1cc8e3
aliciafdezc <cfdezalicia@gmail.com>
2022-11-23 09:52:20 +0100

    route y resource

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+/* -------------- Pruebas -------------- */
+Route::resource('pruebas', pruebasController::class);
+
